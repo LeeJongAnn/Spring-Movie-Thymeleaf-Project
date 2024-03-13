c471ab5201e1fa0feb4c3a9c8a1e8a1b66fde720
LeeJongAnn <dlwhddksiq@naver.com>
2024-03-13 19:33:09 +0900

    Project Setting

---
+++ README.md
@@ -0,0 +1,8 @@
+# SpringBoot-movie-Thymeleaf Project
+
+## use Stack 
+- Spring Boot , Thymeleaf , PostMan 
+
+### Day1
+
+- API Test with RestTemplate
---
+++ src/main/java/com/spring/MovieProject/MovieProjectApplication.java
@@ -0,0 +1,13 @@
+package com.spring.MovieProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieProjectApplication.class, args);
+	}
+
+}
