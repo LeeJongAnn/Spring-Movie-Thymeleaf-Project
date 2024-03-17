410b38ffaaf25fd2f72873bc6288c7955c6ffe77
LeeJongAnn <dlwhddksiq@naver.com>
2024-03-17 19:19:24 +0900

    수정 완료

--- src/main/java/com/spring/MovieProject/response/ApiResponse.java
+++ src/main/java/com/spring/MovieProject/response/ApiResponse.java
@@ -1,5 +1,7 @@
 package com.spring.MovieProject.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
 import lombok.Getter;
 import lombok.ToString;
 
@@ -11,47 +13,48 @@ public class ApiResponse {
 
 
     private int page;
-    private List<ApiResponseResult> results = new ArrayList<>();
+    @JsonProperty("results")
+    private List<ApiResponseResult> movieList = new ArrayList<>();
     private int total_pages;
     private int total_results;
 
-    public List<ApiResponseResult> getListApiResponseResults() {
-        return results;
+    public int getPage() {
+        return page;
     }
 
     public void setPage(int page) {
         this.page = page;
     }
 
-    public void setListApiResponseResults(List<ApiResponseResult> results) {
-        this.results = results;
+    public List<ApiResponseResult> getMovieList() {
+        return movieList;
     }
 
-    public void setTotal_pages(int total_pages) {
-        this.total_pages = total_pages;
-    }
-
-    public void setTotal_results(int total_results) {
-        this.total_results = total_results;
-    }
-
-    public int getPage() {
-        return page;
+    public void setMovieList(List<ApiResponseResult> movieList) {
+        this.movieList = movieList;
     }
 
     public int getTotal_pages() {
         return total_pages;
     }
 
+    public void setTotal_pages(int total_pages) {
+        this.total_pages = total_pages;
+    }
+
     public int getTotal_results() {
         return total_results;
+    }
+
+    public void setTotal_results(int total_results) {
+        this.total_results = total_results;
     }
 
     @Override
     public String toString() {
         return "ApiResponse{" +
                 "page=" + page +
-                ", results=" + results +
+                ", movieList=" + movieList +
                 ", total_pages=" + total_pages +
                 ", total_results=" + total_results +
                 '}';
