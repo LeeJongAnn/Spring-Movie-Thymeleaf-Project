13325906f609d72b28183b5652456dfdf8ab5b89
LeeJongAnn <dlwhddksiq@naver.com>
2024-03-20 23:17:55 +0900

    User에 붙은 어노테이션을 인식하지 못하는 문제 발생해서 수정중

--- src/main/java/com/spring/MovieProject/entity/User.java
+++ src/main/java/com/spring/MovieProject/entity/User.java
@@ -7,9 +7,7 @@ import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
-@Entity
-@Getter
-@Setter
+@Entity(name = "User")
 public class User {
 
     @Id
@@ -38,6 +36,38 @@ public class User {
         this.id = id;
         this.username = username;
         this.email = email;
+        this.password = password;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
         this.password = password;
     }
 
