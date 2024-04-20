8522fcce9f72dbf024773f7790e4f07b38876a02
LeeJongAnn <dlwhddksiq@naver.com>
2024-04-20 22:40:41 +0900

    ### Day 24-04-20 1. UserList 페이지 페이지네이션 추가

--- src/main/java/com/spring/MovieProject/repository/UserRepository.java
+++ src/main/java/com/spring/MovieProject/repository/UserRepository.java
@@ -4,8 +4,9 @@ import com.spring.MovieProject.entity.User;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface UserRepository extends CrudRepository<User, Integer> {
+public interface UserRepository extends CrudRepository<User, Integer>, PagingAndSortingRepository<User,Integer> {
 
      User findByEmail(String email);
 
