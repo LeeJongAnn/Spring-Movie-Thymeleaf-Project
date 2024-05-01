43bc878a6bfc9dd5682cc9792fb156b8467add50
LeeJongAnn <dlwhddksiq@naver.com>
2024-05-01 10:26:50 +0900

    Fix : Error can not find symbols ApiResponseDetailsMovie getId() Bug fix

--- src/main/java/com/spring/MovieProject/response/details/ApiResponseDetailsMovie.java
+++ src/main/java/com/spring/MovieProject/response/details/ApiResponseDetailsMovie.java
@@ -8,9 +8,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-@Getter
-@Setter
-@ToString
+
+
 public class ApiResponseDetailsMovie {
 
     private String adult;
@@ -32,4 +31,108 @@ public class ApiResponseDetailsMovie {
     private float vote_count;
 
 
+    public String getAdult() {
+        return adult;
+    }
+
+    public void setAdult(String adult) {
+        this.adult = adult;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public List<com.spring.MovieProject.response.details.genres> getGenres() {
+        return genres;
+    }
+
+    public void setGenres(List<com.spring.MovieProject.response.details.genres> genres) {
+        this.genres = genres;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public void setOverview(String overview) {
+        this.overview = overview;
+    }
+
+    public String getPopularity() {
+        return popularity;
+    }
+
+    public void setPopularity(String popularity) {
+        this.popularity = popularity;
+    }
+
+    public String getPoster_path() {
+        return poster_path;
+    }
+
+    public void setPoster_path(String poster_path) {
+        this.poster_path = poster_path;
+    }
+
+    public String getRelease_date() {
+        return release_date;
+    }
+
+    public void setRelease_date(String release_date) {
+        this.release_date = release_date;
+    }
+
+    public String getVideo() {
+        return video;
+    }
+
+    public void setVideo(String video) {
+        this.video = video;
+    }
+
+    public float getVote_average() {
+        return vote_average;
+    }
+
+    public void setVote_average(float vote_average) {
+        this.vote_average = vote_average;
+    }
+
+    public float getVote_count() {
+        return vote_count;
+    }
+
+    public void setVote_count(float vote_count) {
+        this.vote_count = vote_count;
+    }
+
+    @Override
+    public String toString() {
+        return "ApiResponseDetailsMovie{" +
+                "adult='" + adult + '\'' +
+                ", id=" + id +
+                ", title='" + title + '\'' +
+                ", genres=" + genres +
+                ", overview='" + overview + '\'' +
+                ", popularity='" + popularity + '\'' +
+                ", poster_path='" + poster_path + '\'' +
+                ", release_date='" + release_date + '\'' +
+                ", video='" + video + '\'' +
+                ", vote_average=" + vote_average +
+                ", vote_count=" + vote_count +
+                '}';
+    }
 }
